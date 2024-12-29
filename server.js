430dc48d7318bed805c8b855921850a3c817c1de
Jay Mehta <69752445+star-warrior@users.noreply.github.com>
2024-12-29 13:48:25 +0530

    Db connect

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 
 const db = new pg.Client({
     user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     port: process.env.POSTGRES_PORT,
